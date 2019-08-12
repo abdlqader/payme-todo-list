57fae66b68860d30381d2631bafcece50f741090
teemoFed74 <42383444+teemoFed74@users.noreply.github.com>
2019-08-12 21:05:52 +0200

    finishing prototype

--- client/src/component/NewTodoList/NewTodoList.js
+++ client/src/component/NewTodoList/NewTodoList.js
@@ -7,7 +7,7 @@ class NewTodoList extends Component
 {
     state = {
         todo:{
-            name:this.formHelper('input','text','Todo Name',''),
+            name:this.formHelper('input','text','Todo Title',''),
             desc:this.formHelper('input','text','What to do ?','')
         },
         loading:false,
@@ -22,6 +22,13 @@ class NewTodoList extends Component
         this.setState({loading:true});
         this.props.history.push('/todo-list');
 
+    }
+    inputChangedHandler = (event , inputIdent) =>{
+        const updatedTodo = {...this.state.todo};
+        const updatedElement = {...updatedTodo[inputIdent]};
+        updatedElement.value = event.target.value;
+        updatedTodo[inputIdent] = updatedElement;
+        this.setState({todo:updatedTodo});
     }
     formHelper (elemType,elemConfigType,elemConfigPlaceHolder,elemValue){
         let elem = {
@@ -47,7 +54,11 @@ class NewTodoList extends Component
             <form onSubmit={this.submitHandler}>
                 <p><strong>New Todo</strong></p>
                 {   formElementArray.map(elem => (
-                            <Input key={elem.id} {...elem.config}></Input>
+                            <Input 
+                            key={elem.id} 
+                            {...elem.config}
+                            changed={(event) => this.inputChangedHandler(event,elem.id)}
+                         ></Input>
                     ))
                 }
                 <Button btnType="Success">Submit</Button>
--- client/src/component/ViewTodoList/ViewTodoList.js
+++ client/src/component/ViewTodoList/ViewTodoList.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
 import classes from './ViewTodoList.css';
 import Todo from './Todo/Todo';
+import axios from 'axios';
+import Spinner from '../UI/Spinner/Spinner';
 class ViewTodoList extends Component {
+    state = {
+        data:null
+    }
+
+    componentDidMount(){
+        axios.get('/api/getall').then(res => {
+            console.log(res.data);
+            this.setState({data:res.data});
+        }).catch(err => err);
+    }
 
     render(){
+        let todos = <Spinner/>
+        if(this.state.data){
+            todos = this.state.data.map(todo =>(
+                //key should be changed bec the logic is not correct
+                <Todo key={todo.title} title={todo.title} desc={todo.desc} />
+            ));
+        }
         return(
             <div className={classes.TodoList}>
-                <Todo title="do it" desc="just do me" />
+                {todos}
             </div>
         );
     }
